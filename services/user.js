040779fbe9b357c909921ecdf1181c9d472fbd88
Rupali Maheshwari <periwal.rupali@gmail.com>
2019-06-26 14:56:11 +0530

    user  details does not contain password now

--- services/user.js
+++ services/user.js
@@ -2,7 +2,13 @@ let userdetails = (req,res,next)=>{
     res.json({
         success : true,
         message : 'successfull',
-        user : req.user
+        user: {
+            name : user.name,
+            type: user.type,
+            _id : user._id,
+            emailid : user.emailid,
+            contact : user.contact
+        }
     })
 }
 
